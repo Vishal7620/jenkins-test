9a0931c9e2b67164d4f696d5ba79fd4b53d900d4
Vishal Patil <vishalpatil18200@gmail.com>
2025-11-11 07:56:41 +0530

    Initial Commit

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class main{
+    public static void main(String[]args)
+    {
+        System.out.println("This is the Java Program");
+        System.out.println("Hello from Java");
+        System.out.println("Current date :"+new Date());
+    }
+}
