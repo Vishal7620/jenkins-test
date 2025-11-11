ba3dd00599ae4960ed76b4c9f63141ddb064f875
Vishal Patil <vishalpatil18200@gmail.com>
2025-11-11 08:48:48 +0530

    added student

---
+++ student.java
@@ -0,0 +1,12 @@
+public class student {
+    
+    public student()
+    {
+      System.out.println("Student objec got iitialized");
+    }
+
+    public void getStudent()
+    {
+        System.out.println("hello from get Student");
+    }
+}
